7025a6bfd46141da22c31d36a56ceb4e738c16fb
JitNub <99720988+JitNub@users.noreply.github.com>
2023-01-26 11:23:05 +0100

    Practicing with
    Property binding and String interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '.app-servers', /* '[app-servers]', */
-  template: `<app-server></app-server> <app-server></app-server>`,
+  selector: '.app-servers' /* '[app-servers]', */,
+  /* template: `<app-server></app-server> <app-server></app-server>`, */
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  testText = 'Då ska vi se om det fungerar.';
+}
