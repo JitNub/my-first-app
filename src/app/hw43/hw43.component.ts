6b0bdd12c564c0c9c5b5cbe6b54673c345cbac5e
JitNub <99720988+JitNub@users.noreply.github.com>
2023-02-07 08:30:35 +0100

    Added .gitignore
    and update to latest Angular

--- src/app/hw43/hw43.component.ts
+++ src/app/hw43/hw43.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hw43',
   templateUrl: './hw43.component.html',
-  styleUrls: ['./hw43.component.css']
+  styleUrls: ['./hw43.component.css'],
 })
 export class Hw43Component {
+  showPassword = false;
+  logToogle = [];
 
+  toogleDetails() {
+    this.showPassword = !this.showPassword;
+
+    this.logToogle.push(this.logToogle.length + 1);
+  }
 }
